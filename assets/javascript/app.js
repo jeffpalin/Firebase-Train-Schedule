0716ea10deace223a9b97c58399f2de8f07660a4
Jeff Palin <jeffpalin@gmail.com>
2017-08-29 20:38:03 -0700

    periodic commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDpShUhcLZv7fYxEftnhqrx3OyoLSQOBjE",
+    authDomain: "first-project-6ba1b.firebaseapp.com",
+    databaseURL: "https://first-project-6ba1b.firebaseio.com",
+    projectId: "first-project-6ba1b",
+    storageBucket: "first-project-6ba1b.appspot.com",
+    messagingSenderId: "801657408458"
+  };
+
+  firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+
+});
